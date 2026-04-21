9f55236c3333b65bc3000944cff64a57bc2856b1
sebi <sebisebpaw@gmail.com>
2026-04-21 18:15:33 -0500

    initial commit

---
+++ HotbarBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using VoidLib2.Inventory;
+using MelonLoader;
+
+namespace InventoryUI
+{
+    [RegisterTypeInIl2Cpp]
+    public class HotbarBehaviour : MonoBehaviour
+    {
+        public HotbarBehaviour(IntPtr ptr) : base(ptr) { }
+
+        public void Update()
+        {
+            if (Hotbar.Instance == null) return;
+            for (int i = 0; i < 9; i++)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+                    Hotbar.Instance.SelectSlot(i);
+            }
+        }
+    }
+}
